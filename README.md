d501593cbcbce84fc9b27f88802554ab4252d9d0
Moisés Jurado <moises003@gmail.com>
2023-10-16 14:55:37 -0500

    v2

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # playwright_2fa_1
 
+## reference
+
+- theme: using playwright with 2fa
+- <https://www.youtube.com/watch?v=849m2cK_Tis>
+- <https://github.com/authlib/otpauth>
+- <https://otp.authlib.org/>
+  - <https://otp.authlib.org/recipes/#use-totp>
+  - <https://otp.authlib.org/recipes/#add-to-authenticator-app>
+- <https://github.com/google/google-authenticator/wiki/Key-Uri-Format>
+- <https://id.heroku.com/login>
+  - <https://verify.salesforce.com/v1/manage/>
+
 ## example
 
-`playwright codegen https://id.heroku.com/login`
+begin with a simple:
+
+- `playwright codegen https://id.heroku.com/login`
--- app2.py
+++ app2.py
@@ -2,6 +2,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 from otpauth import TOTP
 import os
 
+
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
@@ -9,17 +10,19 @@ def run(playwright: Playwright) -> None:
     page.goto("https://id.heroku.com/login")
 
     # ****** page 1 ******
-    page.get_by_placeholder("Email address").fill(os.environ['EMAIL'])    
+    page.get_by_placeholder("Email address").fill(os.environ['EMAIL'])
     page.get_by_placeholder("Password").fill(os.environ['PASS'])
     page.get_by_role("button", name="Log In").click()
 
-    # ****** page 2 ******        
+    # ****** page 2 ******
+
     # Authenticator App #9 | WORKS
     # otpauth://totp/Heroku:moises%20jurado?issuer=Heroku&secret=************SECRET_9************&algorithm=SHA1&digits=6&period=30
     totp = TOTP.from_b32encode(os.environ['SECRET_9'])
-    totp.to_uri("Heroku:moises%20jurado%20#9", "Heroku")
     code: int = totp.generate()
-    
+    uri = totp.to_uri("Heroku:moises jurado #9", "Heroku")
+    # otpauth://totp/Heroku:moises%20jurado%20%239?secret=************SECRET_9************&issuer=Heroku&algorithm=SHA1&digits=6&period=30
+
     page.get_by_label("Verification Code").fill(str(code))
     page.get_by_role("button", name="Verify").click()
 
